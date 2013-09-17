6953c4c8a5874bee49a6d20fcf544ad1dc1d6f06
danasilver <dsilver1221@gmail.com>
2013-09-17 19:05:54 -0400

    restructure file system caching

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,89 +1,90 @@
 $(function() {
     var SERVER = 'localhost:3000';
 
+    var log = console.log.bind(console);
+
     var Instruments = {
         'Piano': ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
     }
 
     window.lookupTable = {};
 
-    // Filesystem
-    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
-
-    window.requestFileSystem(window.TEMPORARY, 5*1024*1024, onInitFs, onError);
-
-    function onInitFs(fs) {
-        for (instrument in Instruments) {
-            var keys = Instruments[instrument],
-                dirPath = "sounds/" + instrument.toLowerCase() + "/";
-            createDir(fs.root, dirPath.split("/"));
-            for (key in keys) {
-                var url = "sounds/" + instrument + "/" + keys[key];
-                getFile(fs, url, dirPath, keys[key], printTable);
-            }
-        }
-    }
-
-    function getFile(fs, url, dirPath, id, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'blob';
-
-        xhr.onload = function(e) {
-            fs.root.getFile(dirPath + id + ".mp3", {create: true}, function(fileEntry) {
-                fileEntry.createWriter(function(writer) {
-                    writer.onwrite = function (e) {
-                        window.lookupTable[id] = fileEntry.toURL();
-                        callback();
-                    }
-                    writer.onerror = function (e) {
-                        onError(e);
-                    }
-
-                    var blob = new Blob([xhr.response], {type: 'audio/mp3'});
-
-                    writer.write(blob);
-                }, onError);
-            }, onError);
-        }
-
-        xhr.send();
-    }
-
     function printTable() {
         console.log(window.lookupTable);
     }
 
-    function readDirectoryContents(fs) {
-        fs.root.getDirectory('sounds/', {create: false}, function(dirEntry){
-            var dirReader = dirEntry.createReader();
-            dirReader.readEntries(function(entries) {
-                for(var i = 0; i < entries.length; i++) {
-                    var entry = entries[i];
-                    if (entry.isDirectory){
-                        console.log('Directory: ' + entry.fullPath);
-                    }
-                    else if (entry.isFile){
-                        console.log('File: ' + entry.fullPath);
-                    }
+    var FileCacher = {
+        requestFileSystem: window.requestFileSystem || window.webkitRequestFileSystem,
+        init: function () {
+            this.requestFileSystem.call(window, window.TEMPORARY, 5*1024*1024, this.onInitFs, this.errorHandler);
+        },
+        onInitFs: function(fs) {
+            var self = FileCacher;
+            for (instrument in Instruments) {
+                var keys = Instruments[instrument],
+                    dirPath = "sounds/" + instrument.toLowerCase() + "/";
+                self.createDir(fs.root, dirPath.split("/"));
+                for (key in keys) {
+                    var url = "sounds/" + instrument + "/" + keys[key];
+                    self.getFile(fs, url, dirPath, keys[key], printTable);
                 }
-            }, onError);
-        }, onError);
-    }   
-
-    function createDir(rootDir, folders) {
-        rootDir.getDirectory(folders[0], {create: true}, function(dirEntry) {
-            if (folders.length) {
-                createDir(dirEntry, folders.slice(1));
             }
-        }, onError);
-    };
+        },
+        getFile: function(fs, url, dirPath, id, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.responseType = 'blob';
 
-    function onError(e) {
-        console.log('Error:', e);
+            xhr.onload = function(e) {
+                fs.root.getFile(dirPath + id + ".mp3", {create: true}, function(fileEntry) {
+                    fileEntry.createWriter(function(writer) {
+                        writer.onwrite = function (e) {
+                            window.lookupTable[id] = fileEntry.toURL();
+                            callback();
+                        }
+                        writer.onerror = function (e) {
+                            onError(e);
+                        }
+
+                        var blob = new Blob([xhr.response], {type: 'audio/mp3'});
+
+                        writer.write(blob);
+                    }, this.errorHandler);
+                }, this.errorHandler);
+            }
+
+            xhr.send();
+        },
+        createDir: function (rootDir, folders) {
+            var self = this;
+            rootDir.getDirectory(folders[0], {create: true}, function(dirEntry) {
+                if (folders.length) {
+                    self.createDir(dirEntry, folders.slice(1));
+                }
+            }, self.errorHandler);
+        },
+        readDirectoryContents: function(fs) {
+            fs.root.getDirectory('sounds/', {create: false}, function(dirEntry){
+                var dirReader = dirEntry.createReader();
+                dirReader.readEntries(function(entries) {
+                    for(var i = 0; i < entries.length; i++) {
+                        var entry = entries[i];
+                        if (entry.isDirectory){
+                            console.log('Directory: ' + entry.fullPath);
+                        }
+                        else if (entry.isFile){
+                            console.log('File: ' + entry.fullPath);
+                        }
+                    }
+                }, this.errorHandler);
+            }, this.errorHandler);
+        },
+        errorHandler: function (e) {
+            log("Filesystem error: ", e);
+        }
     }
 
-    //getFile('sounds/piano/A4', 'A4');
+    FileCacher.init();
 
     var Keys = {
         'a': 97,
